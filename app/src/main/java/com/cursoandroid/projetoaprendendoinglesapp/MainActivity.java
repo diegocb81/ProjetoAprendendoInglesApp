f40d9ba71392ee232e1599c1e1dd5ee3927827b7
Diego Barcellos <diegocb81@gmail.com>
2021-06-09 00:08:14 -0300

    Criando as abas Bichos, Numeros e Vogais

--- app/src/main/java/com/cursoandroid/projetoaprendendoinglesapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/projetoaprendendoinglesapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.cursoandroid.projetoaprendendoinglesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.cursoandroid.projetoaprendendoinglesapp.Fragments.BichosFragment;
+import com.cursoandroid.projetoaprendendoinglesapp.Fragments.NumerosFragment;
+import com.cursoandroid.projetoaprendendoinglesapp.Fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                    .add("Bichos", BichosFragment.class)
+                    .add("Numeros", NumerosFragment.class)
+                    .add("Vogais", VogaisFragment.class)
+                .create()
+        );
     }
 }
