380852fb75f1b258043fccf1c1ae8b028949a034
Diego Barcellos <diegocb81@gmail.com>
2021-06-13 23:47:22 -0300

    Adicionando id's aos mascotes e números

--- app/src/main/java/com/cursoandroid/projetoaprendendoinglesapp/Fragments/BichosFragment.java
+++ app/src/main/java/com/cursoandroid/projetoaprendendoinglesapp/Fragments/BichosFragment.java
@@ -60,7 +60,9 @@ public class BichosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        return view;
     }
 }
